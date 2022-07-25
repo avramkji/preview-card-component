0132fda40ea0c656e47f3ea51c0e0351dfe25634
Avram Kjiprovski <avramkjiprovski@gmail.com>
2022-07-26 01:04:01 +0200

    mobile card initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import mobile_image from './assets/images/image-product-mobile.jpg';
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <img src={mobile_image} alt="Perfume"/>
+      </div>
+      <div>
+        <h2>Perfume</h2>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>
+          A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL.
+        </p>
+        <div>
+          <p>$149.99</p>
+          <p>~~$169.99~~</p>
+        </div>
+        <button>
+          [cart icon]Add to cart
+        </button>
+        
+      </div>
     </div>
   );
 }
