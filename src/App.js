256e44668bb179d41a3b315e2aec8da5654523b1
Avram Kjiprovski <avramkjiprovski@gmail.com>
2022-07-26 19:09:51 +0200

    finalize

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 import mobile_image from './assets/images/image-product-mobile.jpg';
+import {ReactComponent as CartIcon} from './assets/images/icon-cart.svg';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img src={mobile_image} alt="Perfume"/>
-      </div>
-      <div>
-        <h2>Perfume</h2>
-        <h1>Gabrielle Essence Eau De Parfum</h1>
-        <p>
-          A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL.
-        </p>
-        <div>
-          <p>$149.99</p>
-          <p>~~$169.99~~</p>
-        </div>
-        <button>
-          [cart icon]Add to cart
-        </button>
-        
-      </div>
+        <div className='Card'>
+          <img src={mobile_image} alt="Perfume"/>
+          <div className='Card-body-container'>
+            <div className='Card-info'>
+              <h3>Perfume</h3>
+            </div>
+            <div className='Card-body'>
+              <h1>Gabrielle Essence Eau De Parfum</h1>
+              <p>
+                A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL.
+              </p>
+            </div>
+            <div className='Card-price'>
+              <h3 className='price'>$149.99 <span className='old-price'>$169.99</span></h3>
+            </div>
+            <button className='add-to-cart'>
+              <CartIcon className="Cart-Icon"/>  Add to cart
+            </button>
+          </div>
+          </div>
+          
     </div>
   );
 }
